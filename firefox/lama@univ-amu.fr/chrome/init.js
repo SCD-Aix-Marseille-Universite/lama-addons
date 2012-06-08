3911b490c8193fbf6e9d986ab21d23d80ec186cc
llhuillier <laurent.lhuillier@gmail.com>
2012-06-08 12:18:43 +0200

    stuff2

--- firefox/lama@univ-amu.fr/chrome/init.js
+++ firefox/lama@univ-amu.fr/chrome/init.js
@@ -2,18 +2,18 @@ window.addEventListener("load", em_myextensionLoad, true);
 
 function em_myextensionLoad() {
 
-if (document.getElementById("cbutton")) return;
+if (document.getElementById("mybutton")) return;
 
 try {
    var firefoxnav = document.getElementById("nav-bar");
    var curSet = firefoxnav.currentSet;
-   if (curSet.indexOf("cbutton") == -1)
+   if (curSet.indexOf("mybutton") == -1)
    {
      var set;
      if (curSet.indexOf("urlbar-container") != -1)
-       set = firefoxnav.currentSet + ",cbutton";
+       set = firefoxnav.currentSet + ",mybutton";
      else 
-       set = curSet.replace(/urlbar-container/, "cbutton,urlbar-container");
+       set = curSet.replace(/urlbar-container/, "mybutton,urlbar-container");
      firefoxnav.setAttribute("currentset", set);
      firefoxnav.currentSet = set;
      document.persist("nav-bar", "currentset");
